a589ef805483b2fa027560bf23789a995248508d
YESHWANTH. V. <vyeshwanth0621@gmail.com>
2026-03-15 14:22:08 +0530

    sots

--- ss/src/components/ui/Skeleton.tsx
+++
@@ -1,7 +0,0 @@
-import { cn } from '../../lib/cn'
-
-interface SkeletonProps {
-  className?: string
-}
-
-export const Skeleton = ({ className }: SkeletonProps) => <div className={cn('animate-pulse rounded bg-surface', className)} />
--- ss/src/lib/cn.ts
+++
@@ -1,3 +0,0 @@
-import clsx, { type ClassValue } from 'clsx'
-
-export const cn = (...values: ClassValue[]) => clsx(values)
